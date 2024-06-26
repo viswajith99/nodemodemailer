f3a435ffe151ac7e8a60204dd1b94401d600653e
unknown <viswajithharidas111@gmail.com>
2024-06-26 13:13:48 +0530

    updateemailmessage

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000; 
 app.use(bodyParser.json());
 
 const emailRoutes = require('./routes/email');
